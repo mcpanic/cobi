770f5de66eb0f92792a65ff245d2b4d7d4ee2583
Haoqi Zhang <hqz@scooby-doo.csail.mit.edu>
2013-02-03 18:28:09 -0500

    removed redundant transaction update

--- js/db.js
+++ js/db.js
@@ -252,313 +252,313 @@ DB.prototype.loadSchedule = function(){
 	});
 };
 
-DB.prototype.undo = function(uid){
-    $.ajax({
- 	    async: true,
-		type: 'POST',
-		data: { type: 'undo', 
-			uid: uid,
-	         }, 
-		url: "./php/changeSchedule.php",
-		success: function(m){
-		if(m == null){
-		    //alert("You do not have undo privileges");
-		}else{
+// DB.prototype.undo = function(uid){
+//     $.ajax({
+//  	    async: true,
+// 		type: 'POST',
+// 		data: { type: 'undo', 
+// 			uid: uid,
+// 	         }, 
+// 		url: "./php/changeSchedule.php",
+// 		success: function(m){
+// 		if(m == null){
+// 		    //alert("You do not have undo privileges");
+// 		}else{
 		    
-		    // should return something like 
-		    // checkConsistency...
-		}
- 	    },
-	    error : function(m){
-		alert(JSON.stringify(m));
-	    },
-	    dataType: "json"
-	});
-}    
+// 		    // should return something like 
+// 		    // checkConsistency...
+// 		}
+//  	    },
+// 	    error : function(m){
+// 		alert(JSON.stringify(m));
+// 	    },
+// 	    dataType: "json"
+// 	});
+// }    
 
 
-DB.prototype.toggleSlotLock = function(date, time, room, lock, uid){
-    $.ajax({
- 	    async: true,
-		type: 'POST',
-		data: { type: 'lock', 
-			uid: uid,
-		    lock: lock,
-		    date: date,
-		    time: time,
-		    room: room
-		    }, 
-		url: "./php/changeSchedule.php",
-		success: function(m){
-		transactions.push(m);
-		$(document).trigger('transactionUpdate', [transactions[transactions.length -1]]);
- 	    },
-		error : function(m){
-		alert("lock error: " + JSON.stringify(m));
-	    },
-		dataType: "json"
-		});
-}    
+// DB.prototype.toggleSlotLock = function(date, time, room, lock, uid){
+//     $.ajax({
+//  	    async: true,
+// 		type: 'POST',
+// 		data: { type: 'lock', 
+// 			uid: uid,
+// 		    lock: lock,
+// 		    date: date,
+// 		    time: time,
+// 		    room: room
+// 		    }, 
+// 		url: "./php/changeSchedule.php",
+// 		success: function(m){
+// 		transactions.push(m);
+// 		$(document).trigger('transactionUpdate', [transactions[transactions.length -1]]);
+//  	    },
+// 		error : function(m){
+// 		alert("lock error: " + JSON.stringify(m));
+// 	    },
+// 		dataType: "json"
+// 		});
+// }    
 
-    DB.prototype.unscheduleSession = function(id, date, time, room, uid){
-    $.ajax({
- 	    async: true,
-	    type: 'POST',
-	    data: { type: 'unschedule', 
-		    uid: uid,
-		    id: id,
-		    date: date,
-		    time: time,
-		    room: room
-	    }, 
-	    url: "./php/changeSchedule.php",
-	    success: function(m){
-	    	console.log("unscheduleSession success", m);
-		transactions.push(m);
-		$(document).trigger('transactionUpdate', [transactions[transactions.length -1]]);
- 	    },
-	    error : function(m){
-		alert(JSON.stringify(m));
-	    },
-	    dataType: "json"
-	});
-}; 
+//     DB.prototype.unscheduleSession = function(id, date, time, room, uid){
+//     $.ajax({
+//  	    async: true,
+// 	    type: 'POST',
+// 	    data: { type: 'unschedule', 
+// 		    uid: uid,
+// 		    id: id,
+// 		    date: date,
+// 		    time: time,
+// 		    room: room
+// 	    }, 
+// 	    url: "./php/changeSchedule.php",
+// 	    success: function(m){
+// 	    	console.log("unscheduleSession success", m);
+// 		transactions.push(m);
+// 		$(document).trigger('transactionUpdate', [transactions[transactions.length -1]]);
+//  	    },
+// 	    error : function(m){
+// 		alert(JSON.stringify(m));
+// 	    },
+// 	    dataType: "json"
+// 	});
+// }; 
 
-DB.prototype.scheduleSession = function(id, date, time, room, uid){
-    $.ajax({
- 	    async: true,
-	    type: 'POST',
-	    data: { type: 'schedule', 
-		    id: id,
-		    date: date,
-		    time: time,
-		    room: room,
-		    uid: uid}, 
-		url: "./php/changeSchedule.php",
-		success: function(m){		
-		transactions.push(m);
-		$(document).trigger('transactionUpdate', [transactions[transactions.length -1]]);
- 	    },
-		error : function(m){
-		alert(JSON.stringify(m));
-	    },
-		dataType: "json"
-	});
-};
+// DB.prototype.scheduleSession = function(id, date, time, room, uid){
+//     $.ajax({
+//  	    async: true,
+// 	    type: 'POST',
+// 	    data: { type: 'schedule', 
+// 		    id: id,
+// 		    date: date,
+// 		    time: time,
+// 		    room: room,
+// 		    uid: uid}, 
+// 		url: "./php/changeSchedule.php",
+// 		success: function(m){		
+// 		transactions.push(m);
+// 		$(document).trigger('transactionUpdate', [transactions[transactions.length -1]]);
+//  	    },
+// 		error : function(m){
+// 		alert(JSON.stringify(m));
+// 	    },
+// 		dataType: "json"
+// 	});
+// };
 
-DB.prototype.moveSession = function(id, date, time, room, tdate, ttime, troom, uid){
-    $.ajax({
- 	    async: true,
-	    type: 'POST',
-	    data: { type: 'move', 
-		    id: id,
-		    sdate: date,
-		    stime: time,
-		    sroom: room,
-		    tdate: tdate,
-		    ttime: ttime,
-		    troom: troom,
-		    uid: uid}, 
-	    url: "./php/changeSchedule.php",
-	    success: function(m){		
-		transactions.push(m);
-		$(document).trigger('transactionUpdate', [transactions[transactions.length -1]]);
- 	    },
-	    error : function(m){
-		alert(JSON.stringify(m));
-	    },
-	    dataType: "json"
-	});
-};
+// DB.prototype.moveSession = function(id, date, time, room, tdate, ttime, troom, uid){
+//     $.ajax({
+//  	    async: true,
+// 	    type: 'POST',
+// 	    data: { type: 'move', 
+// 		    id: id,
+// 		    sdate: date,
+// 		    stime: time,
+// 		    sroom: room,
+// 		    tdate: tdate,
+// 		    ttime: ttime,
+// 		    troom: troom,
+// 		    uid: uid}, 
+// 	    url: "./php/changeSchedule.php",
+// 	    success: function(m){		
+// 		transactions.push(m);
+// 		$(document).trigger('transactionUpdate', [transactions[transactions.length -1]]);
+//  	    },
+// 	    error : function(m){
+// 		alert(JSON.stringify(m));
+// 	    },
+// 	    dataType: "json"
+// 	});
+// };
 
 
-DB.prototype.swapSession = function(s1id, s1date, s1time, s1room, 
-				    s2id, s2date, s2time, s2room, uid){
+// DB.prototype.swapSession = function(s1id, s1date, s1time, s1room, 
+// 				    s2id, s2date, s2time, s2room, uid){
     
-    $.ajax({
- 	    async: true,
-		type: 'POST',
-		data: { type: 'swap', 
-			s1id: s1id,
-			s1date: s1date,
-			s1time: s1time,
-			s1room: s1room,
-			s2id: s2id,
-			s2date: s2date,
-			s2time: s2time,
-			s2room: s2room,
-			uid: uid
-	    }, 
-	    url: "./php/changeSchedule.php",
-	    success: function(m){
+//     $.ajax({
+//  	    async: true,
+// 		type: 'POST',
+// 		data: { type: 'swap', 
+// 			s1id: s1id,
+// 			s1date: s1date,
+// 			s1time: s1time,
+// 			s1room: s1room,
+// 			s2id: s2id,
+// 			s2date: s2date,
+// 			s2time: s2time,
+// 			s2room: s2room,
+// 			uid: uid
+// 	    }, 
+// 	    url: "./php/changeSchedule.php",
+// 	    success: function(m){
 
-		transactions.push(m);
-		$(document).trigger('transactionUpdate', [transactions[transactions.length -1]]);
- 	    },
-	    error : function(m){
-		alert(JSON.stringify(m));
-	    },
-	    dataType: "json"
-	});
-}
+// 		transactions.push(m);
+// 		$(document).trigger('transactionUpdate', [transactions[transactions.length -1]]);
+//  	    },
+// 	    error : function(m){
+// 		alert(JSON.stringify(m));
+// 	    },
+// 	    dataType: "json"
+// 	});
+// }
 
-DB.prototype.swapWithUnscheduledSession = function(s1id, 
-						   s2id, s2date, s2time, s2room, uid){
+// DB.prototype.swapWithUnscheduledSession = function(s1id, 
+// 						   s2id, s2date, s2time, s2room, uid){
     
-    $.ajax({
- 	    async: true,
-	    type: 'POST',
-	    data: { type: 'swapWithUnscheduled', 
-		    s1id: s1id,
-		    s2id: s2id,
-		    s2date: s2date,
-		    s2time: s2time,
-		    s2room: s2room,
-		    uid: uid
-	    }, 
-	    url: "./php/changeSchedule.php",
-	    success: function(m){
-		transactions.push(m);
-		$(document).trigger('transactionUpdate', [transactions[transactions.length -1]]);
- 	    },
-	    error : function(m){
-		alert(JSON.stringify(m));
-	    },
-	    dataType: "json"
-	});
-}
+//     $.ajax({
+//  	    async: true,
+// 	    type: 'POST',
+// 	    data: { type: 'swapWithUnscheduled', 
+// 		    s1id: s1id,
+// 		    s2id: s2id,
+// 		    s2date: s2date,
+// 		    s2time: s2time,
+// 		    s2room: s2room,
+// 		    uid: uid
+// 	    }, 
+// 	    url: "./php/changeSchedule.php",
+// 	    success: function(m){
+// 		transactions.push(m);
+// 		$(document).trigger('transactionUpdate', [transactions[transactions.length -1]]);
+//  	    },
+// 	    error : function(m){
+// 		alert(JSON.stringify(m));
+// 	    },
+// 	    dataType: "json"
+// 	});
+// }
     
-////// paper level operations
-    DB.prototype.reorderPapers = function(id, newPaperOrder, previousPaperOrder, uid){
-    $.ajax({
-	    async: true,
-	    type: 'POST',
-	    data: { type: 'reorderPapers',
-		    id: id,
-		    newPaperOrder: newPaperOrder.join(),
-		    previousPaperOrder: previousPaperOrder.join(),
-		    uid: uid
-	    }, 
-	    url: "./php/changeSchedule.php",
-	    success: function(m){
-		transactions.push(m);
-		$(document).trigger('transactionUpdate', [transactions[transactions.length -1]]);
- 	    },
-	    error : function(m){
-		alert(JSON.stringify(m));
-	    },
-	    dataType: "json"
-	});
-}
+// ////// paper level operations
+//     DB.prototype.reorderPapers = function(id, newPaperOrder, previousPaperOrder, uid){
+//     $.ajax({
+// 	    async: true,
+// 	    type: 'POST',
+// 	    data: { type: 'reorderPapers',
+// 		    id: id,
+// 		    newPaperOrder: newPaperOrder.join(),
+// 		    previousPaperOrder: previousPaperOrder.join(),
+// 		    uid: uid
+// 	    }, 
+// 	    url: "./php/changeSchedule.php",
+// 	    success: function(m){
+// 		transactions.push(m);
+// 		$(document).trigger('transactionUpdate', [transactions[transactions.length -1]]);
+//  	    },
+// 	    error : function(m){
+// 		alert(JSON.stringify(m));
+// 	    },
+// 	    dataType: "json"
+// 	});
+// }
     
-DB.prototype.swapPapers = function(s1id, p1id, s2id, p2id, uid){
-    $.ajax({
-	    async: true,
-	    type: 'POST',
-	    data: { type: 'swapPapers',
-		    s1id: s1id,
-		    p1id: p1id,
-		    s2id: s2id,
-		    p2id: p2id,
-		    uid: uid
-	    }, 
-	    url: "./php/changeSchedule.php",
-	    success: function(m){
-		transactions.push(m);
-		$(document).trigger('transactionUpdate', [transactions[transactions.length -1]]);
- 	    },
-	    error : function(m){
-		alert(JSON.stringify(m));
-	    },
-	    dataType: "json"
-	});
-}
+// DB.prototype.swapPapers = function(s1id, p1id, s2id, p2id, uid){
+//     $.ajax({
+// 	    async: true,
+// 	    type: 'POST',
+// 	    data: { type: 'swapPapers',
+// 		    s1id: s1id,
+// 		    p1id: p1id,
+// 		    s2id: s2id,
+// 		    p2id: p2id,
+// 		    uid: uid
+// 	    }, 
+// 	    url: "./php/changeSchedule.php",
+// 	    success: function(m){
+// 		transactions.push(m);
+// 		$(document).trigger('transactionUpdate', [transactions[transactions.length -1]]);
+//  	    },
+// 	    error : function(m){
+// 		alert(JSON.stringify(m));
+// 	    },
+// 	    dataType: "json"
+// 	});
+// }
 
-DB.prototype.swapWithUnscheduledPaper = function(p1id, s2id, p2id, uid){
-    $.ajax({
-	    async: true,
-	    type: 'POST',
-	    data: { type: 'swapWithUnscheduledPaper',
-		    p1id: p1id,
-		    s2id: s2id,
-		    p2id: p2id,
-		    uid: uid
-	    }, 
-	    url: "./php/changeSchedule.php",
-	    success: function(m){
-		transactions.push(m);
-		$(document).trigger('transactionUpdate', [transactions[transactions.length -1]]);
- 	    },
-	    error : function(m){
-		alert(JSON.stringify(m));
-	    },
-	    dataType: "json"
-	});
-}
+// DB.prototype.swapWithUnscheduledPaper = function(p1id, s2id, p2id, uid){
+//     $.ajax({
+// 	    async: true,
+// 	    type: 'POST',
+// 	    data: { type: 'swapWithUnscheduledPaper',
+// 		    p1id: p1id,
+// 		    s2id: s2id,
+// 		    p2id: p2id,
+// 		    uid: uid
+// 	    }, 
+// 	    url: "./php/changeSchedule.php",
+// 	    success: function(m){
+// 		transactions.push(m);
+// 		$(document).trigger('transactionUpdate', [transactions[transactions.length -1]]);
+//  	    },
+// 	    error : function(m){
+// 		alert(JSON.stringify(m));
+// 	    },
+// 	    dataType: "json"
+// 	});
+// }
 
-DB.prototype.movePaper = function(s1id, p1id, s2id, uid){
-    $.ajax({
-	    async: true,
-	    type: 'POST',
-	    data: { type: 'movePaper',
-		    s1id: s1id,
-		    p1id: p1id,
-		    s2id: s2id,
-		    uid: uid
-	    }, 
-	    url: "./php/changeSchedule.php",
-	    success: function(m){
-		transactions.push(m);
-		$(document).trigger('transactionUpdate', [transactions[transactions.length -1]]);
- 	    },
-	    error : function(m){
-		alert(JSON.stringify(m));
-	    },
-	    dataType: "json"
-	});
-}
+// DB.prototype.movePaper = function(s1id, p1id, s2id, uid){
+//     $.ajax({
+// 	    async: true,
+// 	    type: 'POST',
+// 	    data: { type: 'movePaper',
+// 		    s1id: s1id,
+// 		    p1id: p1id,
+// 		    s2id: s2id,
+// 		    uid: uid
+// 	    }, 
+// 	    url: "./php/changeSchedule.php",
+// 	    success: function(m){
+// 		transactions.push(m);
+// 		$(document).trigger('transactionUpdate', [transactions[transactions.length -1]]);
+//  	    },
+// 	    error : function(m){
+// 		alert(JSON.stringify(m));
+// 	    },
+// 	    dataType: "json"
+// 	});
+// }
 
-DB.prototype.unschedulePaper = function(sid, pid, uid){
-    $.ajax({
-	    async: true,
-	    type: 'POST',
-	    data: { type: 'unschedulePaper',
-		    sid: sid,
-		    pid: pid,
-		    uid: uid
-	    }, 
-	    url: "./php/changeSchedule.php",
-	    success: function(m){
-		transactions.push(m);
-		$(document).trigger('transactionUpdate', [transactions[transactions.length -1]]);
- 	    },
-	    error : function(m){
-		alert(JSON.stringify(m));
-	    },
-	    dataType: "json"
-	});
-}
+// DB.prototype.unschedulePaper = function(sid, pid, uid){
+//     $.ajax({
+// 	    async: true,
+// 	    type: 'POST',
+// 	    data: { type: 'unschedulePaper',
+// 		    sid: sid,
+// 		    pid: pid,
+// 		    uid: uid
+// 	    }, 
+// 	    url: "./php/changeSchedule.php",
+// 	    success: function(m){
+// 		transactions.push(m);
+// 		$(document).trigger('transactionUpdate', [transactions[transactions.length -1]]);
+//  	    },
+// 	    error : function(m){
+// 		alert(JSON.stringify(m));
+// 	    },
+// 	    dataType: "json"
+// 	});
+// }
     
-DB.prototype.schedulePaper = function(sid, pid, uid){
-    $.ajax({
-	    async: true,
-	    type: 'POST',
-	    data: { type: 'schedulePaper',
-		    sid: sid,
-		    pid: pid,
-		    uid: uid
-	    }, 
-	    url: "./php/changeSchedule.php",
-	    success: function(m){
-		transactions.push(m);
-		$(document).trigger('transactionUpdate', [transactions[transactions.length -1]]);
- 	    },
-	    error : function(m){
-		alert(JSON.stringify(m));
-	    },
-	    dataType: "json"
-	});
-}
+// DB.prototype.schedulePaper = function(sid, pid, uid){
+//     $.ajax({
+// 	    async: true,
+// 	    type: 'POST',
+// 	    data: { type: 'schedulePaper',
+// 		    sid: sid,
+// 		    pid: pid,
+// 		    uid: uid
+// 	    }, 
+// 	    url: "./php/changeSchedule.php",
+// 	    success: function(m){
+// 		transactions.push(m);
+// 		$(document).trigger('transactionUpdate', [transactions[transactions.length -1]]);
+//  	    },
+// 	    error : function(m){
+// 		alert(JSON.stringify(m));
+// 	    },
+// 	    dataType: "json"
+// 	});
+// }
     
 
     
