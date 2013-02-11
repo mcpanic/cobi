e569a24717d9c4c62a0023ea9a17b6bc109dbd7a
Haoqi Zhang <hqz@scooby-doo.csail.mit.edu>
2013-02-11 12:45:32 -0500

    j

--- js/CCOps.js
+++ js/CCOps.js
@@ -7,10 +7,44 @@ function SingleEntityConstraint(type, description, importance, rationale, entity
     this.type = type;
 }
 
-function Rule(keyHierarchy, comp, value){
+function entityTrace(session, submission, author){
+    this.session = session;
+    this.submission = submission;
+    this.author = author;
+}
+
+// level == session
+// level == submission
+// level == author
+function exist(s, level, comp){
+    var matches = [];
+    if(level == 'session'){
+	if(comp(s)){
+	    matches.push(new entityTrace(s.id, null, null));
+	}
+	return matches;
+    }else if(level == 'submission'){
+	for(var sub in s.submissions){
+	    if(comp(s.submissions[sub])){
+		matches.push(new entityTrace(s.id, sub, null));
+	    }
+	}
+	return matches;
+    }else if(level == 'author'){
+	for(var sub in s.submissions){
+	    for(var auth in s.submissions[sub].authors){
+		if(comp(s.submissions[sub].authors[auth])){
+		    matches.push(new entityTrace(s.id, sub, auth));
+		}
+	    }
+	}
+	return matches;
+    }
+}
+
+function Rule(keyHierarchy, comp){
     this.keyHierarchy = keyHierarchy;
     this.comp = comp;
-    this.value = value;
 }
 
 var CCOps = function(){
@@ -24,12 +58,85 @@ var CCOps = function(){
 	var violatedRules = [];
 	
 	for (var i in rules){
-	    if(!sessionMatchesPred(x, rules[i].keyHierarchy, rules[i].comp, rules[i].value)){
+	    if(!sessionMatchesPred(x, rules[i].keyHierarchy, rules[i].comp)){
 		violatedRules.push(rules[i]);
 	    }
 	}
 	return violatedRules;
     }
+function multiLevel(rules){
+    var matches = [];
+    var levels = {};
+    levels['session'] = [];
+    levels['submission'] = [];
+    levels['author'] = [];
+    
+    for(var i = 0; i < rules.length; i++){
+	levels[levels[rules[i].level]].push(rules[i]);
+    }
+    // define all paths
+    var paths = [];
+
+    for(var s in allSessions){
+	if(levels['submission'].length != 0 || levels['author'].length != 0){
+	    for(var sub in allSessions[s].submissions){
+		if(levels['author'].length != 0){
+		    for(var auth in allSessions[s].submissions[sub].authors){
+			paths.push(new entityTrace(s, sub, auth));
+		    }
+		}else{
+		    // just generate sub level paths
+		    paths.push(new entityTrace(s, sub, null));
+		}
+	    }
+	}else{
+	    paths.push(new entityTrace(s, null, null));
+	}
+    }
+    
+    var legal = [];
+    for(var i in paths){
+	legal.push(true);
+    }
+    // filter through level by level
+    for(var i in paths){
+	if(legal[i]){
+	    for(var sessionRule in levels['session']){
+		if(!(levels['session'][sessionRule].comp)(allSessions[paths[i].session])){
+		    legal[i] = false;
+		}
+	    }
+	}
+    }
+
+    for(var i in paths){
+	if(legal[i]){
+	    for(var submissionRule in levels['submission']){
+		if(!(levels['submission'][submissionRule].comp)(allSessions[paths[i].session].submissions[paths[i].submission])){
+		    legal[i] = false;
+		}
+	    }
+	}
+    }
+
+    for(var i in paths){
+	if(legal[i]){
+	    for(var authorRule in levels['author']){
+		if(!(levels['author'][authorRule].comp)(allSessions[paths[i].session].submissions[paths[i].submission].authors[paths[i].author])){
+		    legal[i] = false;
+		}
+	    }
+	}
+    }
+    
+    var legalPaths = [];
+    for (var i in paths){
+	if(legal[i]){
+	    legalPaths.push(paths[i])
+	}
+    }
+    return legalPaths;
+}
     
     function sessionMatchesPred(x, keyHierarchy, comp, value){
 	var o = x;
@@ -91,6 +198,7 @@ var CCOps = function(){
 	    sessionMatchesPred: sessionMatchesPred,
 	    getAllMatchingSessions: getAllMatchingSessions,
 	    belongs: belongs,
+	    multiLevel: multiLevel,
 	    equal: equal};
 }();
 
